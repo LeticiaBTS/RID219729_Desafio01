3ffbc590fc116ec60896a288f974f6ebc4ed5d45
LeticiaBTS <barbosa.leticia753@gmail.com>
2025-09-24 17:48:09 -0300

    organiza links e  alteração no js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # RID219729_Desafio01
-Este é um projeto de desenvolvimento para a conclusão do curso "Desenvolvedor Back-End" da  escola DNC
-Link do site disponibilizado: https://rid219729.netlify.app/
+Este é um projeto de desenvolvimento para a conclusão do curso "Desenvolvedor Back-End" da  escola DNC<br>
+Link do site disponibilizado: https://rid219729.netlify.app/<br>
+Link da planilha de registro de informação: https://docs.google.com/spreadsheets/d/17WE9DgpTGSF5PKWzOXhe40MZXF6KSbkIvoCFwxLGOqQ/edit?gid=0#gid=0
 
--- index.js
+++ index.js
@@ -15,33 +15,3 @@ function revelarAoRolar() {
 
 
 window.addEventListener("scroll", revelarAoRolar);
-
-// Preenche o formuláro e não sai da página
-revelarAoRolar();
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function(e) {
-  e.preventDefault(); 
-
-
-  const formData = new FormData(form);
-
-  
-  fetch(form.action, {
-    method: "POST",
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      alert("✅ Obrigado! Em breve entraremos em contato.");
-      form.reset();
-    } else {
-      alert("❌ Ocorreu um erro, tente novamente.");
-    }
-  })
-  .catch(() => {
-    alert("❌ Não foi possível enviar. Verifique sua conexão.");
-  });
-});
-
